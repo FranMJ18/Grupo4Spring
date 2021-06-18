49601057c5fc0058c97fc185e569939dfbe258de
FranMJ18 <franciscomj00@uma.es>
2021-06-18 18:22:10 +0200

    imagenes + estilos

--- src/main/java/es/taw/grupo4/controller/AppController.java
+++ src/main/java/es/taw/grupo4/controller/AppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
     @GetMapping("/")
-    public String init(){
+    public String doInit(){
         return "index";
     }
 }
